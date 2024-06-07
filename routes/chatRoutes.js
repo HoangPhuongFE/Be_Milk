0312ebf9acf285dedb767d007cc0374e7524a106
GiaiCuuHoang <hoang093898xxx@gmail.com>
2024-06-08 00:36:55 +0700

    add Chat

--- routes/chatRoutes.js
+++ routes/chatRoutes.js
@@ -1,9 +1,1 @@
-const express = require('express');
-const router = express.Router();
-const chatController = require('../controllers/chatController');
-const { authenticateToken } = require('../middlewares/auth');
-
-router.post('/', authenticateToken, chatController.createMessage);
-router.get('/', authenticateToken, chatController.getMessages);
-
-module.exports = router;
+//aa
