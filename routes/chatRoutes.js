c9fe9b58d4636e4d5de64e9435d457e7a75d7914
GiaiCuuHoang <hoang093898xxx@gmail.com>
2024-06-08 00:37:23 +0700

    fix chat

--- routes/chatRoutes.js
+++ routes/chatRoutes.js
@@ -1,1 +1,9 @@
-//aa
+const express = require('express');
+const router = express.Router();
+const chatController = require('../controllers/chatController');
+const { authenticateToken } = require('../middlewares/auth');
+
+router.post('/', authenticateToken, chatController.createMessage);
+router.get('/', authenticateToken, chatController.getMessages);
+
+module.exports = router;
