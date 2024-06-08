5cf1fe739e95a399870630b32adaffc236ea71c9
GiaiCuuHoang <hoang093898xxx@gmail.com>
2024-06-08 13:24:02 +0700

    add middleware

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const reviewController = require('../controllers/reviewController');
-const { authenticateToken } = require('../middlewares/auth');
+//const { authenticateToken } = require('../middlewares/auth');
+const authenticateToken = require('../middleware/authenticateToken');
 
 router.post('/', authenticateToken, reviewController.createReview);
 router.get('/:product_id', reviewController.getReviews);
