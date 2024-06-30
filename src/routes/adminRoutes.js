44c1f31303fbb46202988a038a3b4a9ee5c718c8
GiaiCuuHoang <hoang093898xxx@gmail.com>
2024-07-01 03:16:26 +0700

    fix routes for admin , review

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -4,13 +4,12 @@ const userController = require('../controllers/userController');
 const { authenticateToken, authorizeRole } = require('../middlewares/auth');
 
 // Lấy tất cả người dùng (Chỉ dành cho Admin)
-router.get('/users', authenticateToken, authorizeRole(['admin']), userController.getAllUsers);
+router.get('/all', authenticateToken, authorizeRole(['admin','staff']), userController.getAllUsers);
 
 // Phân quyền người dùng (Chỉ dành cho Admin)
 router.post('/assign-role', authenticateToken, authorizeRole(['admin']), userController.assignRole);
 
 // Xóa người dùng (Chỉ dành cho Admin)
-router.delete('/users/:id', authenticateToken, authorizeRole(['admin']), userController.deleteUser);
+router.delete('/users/:id', authenticateToken, authorizeRole(['admin','staff']), userController.deleteUser);
 
 module.exports = router;
-///
