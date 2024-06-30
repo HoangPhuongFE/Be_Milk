69b22d9b029bbe4b3ca37e4fe72126da2825342e
GiaiCuuHoang <hoang093898xxx@gmail.com>
2024-07-01 00:08:04 +0700

    add mysql / config

--- src/config/config.js
+++ src/config/config.js
@@ -1,25 +1,30 @@
 require('dotenv').config();
 
+const mysql2 = require('mysql2');
+
 module.exports = {
   development: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT
+    dialect: process.env.DB_DIALECT,
+    dialectModule: mysql2
   },
   test: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: `${process.env.DB_DATABASE}_test`,
     host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT
+    dialect: process.env.DB_DIALECT,
+    dialectModule: mysql2
   },
   production: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: `${process.env.DB_DATABASE}_prod`,
     host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT
+    dialect: process.env.DB_DIALECT,
+    dialectModule: mysql2
   }
 };
